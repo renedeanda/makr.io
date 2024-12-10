724e766e7c850560fd02eec6a9cfbcfecaf670f5
renedeanda <rene.ideanda@gmail.com>
2024-12-10 00:09:34 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,13 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; img-src 'self' data: https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com;"
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: https://www.google-analytics.com https://api.producthunt.com",
+              "connect-src 'self' https://www.google-analytics.com"
+            ].join('; ')
           },
         ],
       },
